522de54c14a28041c0928f2e88010e59491f7b50
hirohiko <hirohiko.work@gmail.com>
2023-10-15 23:08:27 +0900

    index' link-list fixed,lesson01 fininshed

--- 1/index.js
+++ 1/index.js
@@ -1,1 +1,5 @@
 console.log("No.1");
+// Main-Program
+const list = document.querySelector("main > ul");
+const listChild_node = "<li>これです</li>";
+list.insertAdjacentHTML("afterbegin", listChild_node);
