a8df861fc053654561dc6aee53af97b8d5b22632
hirohiko <hirohiko.work@gmail.com>
2023-10-20 16:22:38 +0900

    lesson2-finished

--- 2/index.js
+++ 2/index.js
@@ -1,5 +1,14 @@
-console.log("No.1");
+console.log("No.2");
 // Main-Program
+// node作成
 const list = document.querySelector("main > ul");
-const listChild_node = "<li>これです</li>";
-list.insertAdjacentHTML("afterbegin", listChild_node);
+const listChild_node = document.createElement("li");
+const alink_node = document.createElement("a");
+const img_node = document.createElement("img");
+
+listChild_node.textContent = "これです";
+alink_node.setAttribute("href", "1.html");
+img_node.setAttribute("src", "bookmark.png");
+img_node.setAttribute("alt", "ブックマーク");
+
+list.appendChild(listChild_node).appendChild(alink_node).appendChild(img_node);
