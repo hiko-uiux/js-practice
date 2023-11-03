c19d1b0843b8ba5e5a2360ec90c27fa1f8a29493
hirohiko <hirohiko.work@gmail.com>
2023-11-04 01:32:44 +0900

    style: return to the init by Delete-files

--- 1/index.js
+++
@@ -1,5 +0,0 @@
-console.log("No.1");
-// Main-Program
-const list = document.querySelector("main > ul");
-const listChild_node = "<li>これです</li>";
-list.insertAdjacentHTML("afterbegin", listChild_node);
