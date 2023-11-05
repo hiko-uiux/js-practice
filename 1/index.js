aa95af06d4039c99d4e51f59bb9a2c054a4e3555
hiko-uiux <100347012+hiko-uiux@users.noreply.github.com>
2023-11-06 01:48:03 +0900

    Revert "Lesson1・Completion report"

--- 1/index.js
+++
@@ -1,6 +0,0 @@
-// DOM-select
-const list = document.getElementById("js-target");
-// NODE-create
-const listChild = "<li>これです</li>";
-// NODE-insert
-list.insertAdjacentHTML("afterbegin", listChild);
