43c24477e037449c2847be923d068376a052ab9b
hirohiko <hirohiko.work@gmail.com>
2023-11-07 22:19:44 +0900

    feat: lesson1-data finished

---
+++ 1/index.js
@@ -0,0 +1,6 @@
+// DOM-select
+const list = document.getElementById("js-target");
+// NODE-create
+const listChild = "<li>これです</li>";
+// NODE-insert
+list.insertAdjacentHTML("afterbegin", listChild);
