bc51e75add3f3956a6fcdce043190d7e31659d0a
hirohiko <hirohiko.work@gmail.com>
2023-12-24 00:10:24 +0900

    refactor: Improved code readability

--- 2/index.js
+++ 2/index.js
@@ -1,35 +1,30 @@
-// Must1:DOM-select
+// DOM-select
 const list = document.getElementById("target");
 
-// Must2:NODE-create
-const infoList = {
+// Information
+const infoList1 = {
   li: "",
   a: { href: "1.html", text: "これです" },
   img: { src: "bookmark.png", alt: "ブックマーク" },
 };
 
-const propNames = Object.keys(infoList);
-let nodeList = [];
-propNames.forEach((item) => {
-  nodeList.push(document.createElement(item));
-});
-
-// Want:setAttr-function
-function addAttr(num, attr) {
-  nodeList[num].setAttribute(attr, infoList[propNames[num]][attr]);
-  return nodeList[num];
+// Function: create-Node
+function createNode(element) {
+  return document.createElement(element);
 }
-// img-create
-let imgNode = addAttr(2, "src");
-imgNode = addAttr(2, "alt");
-// a-create
-let aNode = addAttr(1, "href");
-aNode.appendChild(imgNode);
-const aNodeText = document.createTextNode(infoList.a.text);
-aNode.append(aNodeText);
-// li-create
-nodeList[0].appendChild(aNode);
-let liNode = nodeList[0];
 
-// Must3:NODE-insert
+// create-Node
+const liNode = createNode("li");
+const aNode = createNode("a");
+const aNodeText = document.createTextNode(infoList1.a.text);
+const imgNode = createNode("img");
+// add option
+imgNode.setAttribute("src", infoList1.img.src);
+imgNode.setAttribute("alt", infoList1.img.alt);
+aNode.setAttribute("href", infoList1.a.href);
+
+// insert-Node
+aNode.appendChild(imgNode);
+aNode.append(aNodeText);
+liNode.appendChild(aNode);
 list.appendChild(liNode);
